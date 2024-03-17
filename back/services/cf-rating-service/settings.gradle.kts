ff7ea119b8f4c685151e7be483e94459a67ff5fa
Kirill Popov <pajehyk@gmail.com>
2024-03-17 22:21:37 +0300

    Added kotlin cf-rating-service, added Codeforces OpenAPI spec, generated client lib.

--- back/services/cf-rating-service/settings.gradle.kts
+++
@@ -1,1 +0,0 @@
-rootProject.name = "cf-rating-service"
---
+++ back/settings.gradle.kts
@@ -0,0 +1,5 @@
+rootProject.name = "cf-rating-distribution"
+
+include("apis")
+include("services:cf-rating-service")
+include("libraries:codeforces-client")
